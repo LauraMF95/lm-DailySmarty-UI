99ed432e5ed7235120307d656d6dc7cecd3e88b4
Laura <laumafer_16@hotmail.com>
2024-08-29 12:56:19 +0200

    used axios to perform get request to fetch recents posts and setup the required reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,20 @@
+import { SET_RECENT_POSTS } from "./types";
+
+import axios from "axios";
+
 export function fetchRecentPosts() {
     return function(dispatch) {
-        //perform our request here
-        console.log("hello");
+        axios
+            .get("https://swapi.dev/api/")
+            .then(response => {
+                console.log(response.data);
+                dispatch({
+                    type: SET_RECENT_POSTS,
+                    payload: response.data
+                })
+            })
+            .catch(error => {
+                console.log("fetchRecentPosts error", error);
+            })
     }
 }
